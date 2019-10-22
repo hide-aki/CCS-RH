f5b8fa213a2312e928526607ed8b3a762526a1a2
Isaac Contreras <iacontrerasg@icloud.com>
2019-10-22 11:45:47 -0500

    Polyfil Calendario

--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,17 @@ import React from "react";
 
 const Inicio = React.lazy(() => import("./views/Inicio"));
 const Dashboard = React.lazy(() => import("./views/Dashboard"));
+const Forms = React.lazy(() => import("./views/Forms"));
+const Reclutamiento = React.lazy(() => import("./views/Reclutamiento"));
 
 var routes = [];
 
-// eslint-disable-next-line no-self-compare
-if (1 == 1) {
-  routes = [
-    { path: "/", exact: true, name: "Home" },
-    { path: "/Inicio", name: "Inicio", component: Inicio },
-    { path: "/Dashboard", name: "Dashboard", component: Dashboard }
-  ];
-} else{
-  routes = [
-    { path: "/", exact: true, name: "Home" },
-    { path: "/Inicio", name: "Inicio", component: Inicio },
-    { path: "/Dashboard", name: "Dashboard", component: Dashboard }
-  ];
-}
+routes = [
+  { path: "/", exact: true, name: "Home" },
+  { path: "/Inicio", name: "Inicio", component: Inicio },
+  { path: "/Dashboard", name: "Dashboard", component: Dashboard },
+  { path: "/Forms", name: "Forms", component: Forms },
+  { path: "/Reclutamiento", name: "Reclutamiento", component: Reclutamiento }
+];
 
 export default routes;
